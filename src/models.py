1d30a0b0fe46fdd8fc26cbf7fa37647c7dd3c890
Javiera Vega <javieravegareyes7@gmail.com>
2025-06-03 19:54:10 +0000

    class Media correction

--- src/models.py
+++ src/models.py
@@ -1,37 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Table, Column, ForeignKey
+from sqlalchemy import String, Boolean, Table, Column, ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
-from enum import Enum
+import enum 
 
 db = SQLAlchemy()
 
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class TypeMedia(enum.Enum):
+    VIDEO = "video"
+    IMAGE = "image"
 
 
 association_table = Table(
     "association",
     db.metadata,
-    Column("users_id", ForeignKey("users.id"), primary_key=True),
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
     Column("follower_id", ForeignKey("follower.id"), primary_key=True)
 )
 
-
-class Users(db.Model):
-    __tablename__ = "users"
+class User(db.Model):
+    __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), nullable=False)
@@ -41,75 +29,58 @@ class Users(db.Model):
 
     followers: Mapped[List["Follower"]] = relationship(
         "Follower",
-        secondary=association_table,
-        back_populates="userstwo"
-    )
+        secondary=association_table, back_populates="usertwo")
     posts: Mapped[List["Post"]] = relationship(
-        back_populates="users_post"
-    )
+        back_populates="user_post")
     digitalmedia: Mapped[List["Media"]] = relationship(
-        back_populates="users_media"
-    )
+        back_populates="user_media")
     comments: Mapped[List["Comment"]] = relationship(
-        back_populates="comment_users"
-    )
-
+        back_populates="comment_user")
 
 class Follower(db.Model):
     __tablename__ = "follower"
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    userstwo: Mapped[List["Users"]] = relationship(
-        "Users",
-        secondary=association_table,
-        back_populates="followers"
-    )
-
+    usertwo: Mapped[List["User"]] = relationship(
+        "User",
+        secondary=association_table, back_populates="followers")
 
 class Post(db.Model):
     __tablename__ = "post"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
 
-    users_post: Mapped["Users"] = relationship(
-        back_populates="posts"
-    )
+    user_post: Mapped["User"] = relationship(
+        back_populates="posts")
     list_media: Mapped[List["Media"]] = relationship(
-        back_populates="post_media"
-    )
+        back_populates="post_media")
     list_comment: Mapped[List["Comment"]] = relationship(
-        back_populates="comment_post"
-    )
-
+        back_populates="comment_post")
 
 class Media(db.Model):
     __tablename__ = "media"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[enum] = mapped_column(Enum(TypeMedia), nullable=False)
     url: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
 
-    users_media: Mapped["Users"] = relationship(
-        back_populates="digitalmedia"
-    )
+    user_media: Mapped["User"] = relationship(
+        back_populates="digitalmedia")
     post_media: Mapped["Post"] = relationship(
-        back_populates="list_media"
-    )
-
+        back_populates="list_media")
 
 class Comment(db.Model):
     __tablename__ = "comment"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     comment_text: Mapped[str] = mapped_column(String(2000), nullable=False)
-    users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
 
     comment_post: Mapped["Post"] = relationship(
-        back_populates="list_commment"
-    )
-    comment_users: Mapped["Users"] = relationship(
-        back_populates="comments"
-    )
+        back_populates="list_commment")
+    comment_user: Mapped["User"] = relationship(
+        back_populates="comments")
